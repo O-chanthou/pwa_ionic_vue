053e247888058ace9d1fa26ef2785a1e48a71fbb
O-chanthou <o.chanthou21@gmail.com>
2023-04-28 16:00:38 +0700

    initial

---
+++ src/views/pwa-reload/ReloadPWA.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="needRefresh" class="alert-reload" role="alert">
+    <div class="message">
+      <span v-if="needRefresh">
+        New content available, click on reload button to update.
+      </span>
+    </div>
+    <div class="btn-container">
+      <button v-if="needRefresh" class="btn-reload" @click="updateServiceWorker()">
+        Reload
+      </button>
+      <button @click="close" class="btn-close">Close</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRegisterSW } from "virtual:pwa-register/vue";
+const { needRefresh, updateServiceWorker } = useRegisterSW();
+
+const close = () => {
+  needRefresh.value = false;
+}
+</script>
+
+<style scoped>
+.alert-reload {
+  width: auto;
+  background: #abdbe3;
+  border-radius: 8px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-between;
+}
+.message {
+  color: black;
+  font-size: 0.8em;
+}
+.btn-container {
+  display: flex;
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+}
+button {
+  padding: 6px;
+  border-radius: 6px;
+}
+.btn-reload {
+  color: #fff;
+  font-size: 0.6em;
+  background-color:cornflowerblue;
+}
+.btn-close {
+  font-size: 0.6em;
+  color: #fff;
+  background: #000;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      "mode": "development",
+      "base": "/",
+      "srcDir": "src",
+      "filename": "sw.ts",
+      "includeAssets": ["/favicon.png"],
+      "strategies": "injectManifest",
+      "manifest": {
+        "name": "Project PWA",
+        "short_name": "PWA-Test",
+        "theme_color": "#ffffff",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "icons": [
+          {
+            "src": "./img/icons/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "./img/icons/android-chrome-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      },
+    }),
+  ],
+});
